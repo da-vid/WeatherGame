205a90f7239582f52220ee46627dd4110aab8b57
da-vid <dave.wong@gmail.com>
2014-02-10 21:37:03 -0800

    Squashed AJAX bug, cleaned up code

--- js/weatherGame.js
+++ js/weatherGame.js
@@ -9,7 +9,6 @@ quicklist.controller('gameController', function($scope, $http){
                     "Denver", "Houston", "Las Vegas", "Los Angeles", "Memphis", "Minneapolis", "Miami", "New Orleans",
                      "New York", "Phoenix", "Salt Lake City", "San Diego", "San Francisco", "Tampa", "Washington DC"];
 
-    // Mostly unnecessary variable declaration
     $scope.status = "";
     $scope.correctCityWeather = {};
     $scope.correctCityIdx =  0;
@@ -19,7 +18,7 @@ quicklist.controller('gameController', function($scope, $http){
     $scope.round = 0;
     $scope.score = 0;
     $scope.bestScore = 0;
-    $scope.ajaxLoading = true;
+    ajaxLoading = true;
     $scope.waitForAjax = true;        
     guessTaken = false;
     nextCityIdx =  0; 
@@ -27,28 +26,41 @@ quicklist.controller('gameController', function($scope, $http){
     nextOptions = [];
     shownCities = [];
 
-    $scope.startGame = function startGame() {
+    $scope.startGame = function() {
         $scope.round = 0;
         $scope.score = 0;
         shownCities.length = 0;
         $scope.beginRound();
     };
 
-    $scope.beginRound = function beginRound() {
-        if($scope.ajaxLoading) {
+    $scope.beginRound = function() {        
+        $scope.round++;    
+        $scope.gameState = "question";      
+        if(ajaxLoading) {
             $scope.waitForAjax = true;
         }
-        $scope.round++;    
+        checkForAjax();
+    };
+
+    function beginRoundPart2() {
         guessTaken = false;
         $scope.correctCityWeather = nextCityWeather;
         $scope.correctCityIdx = nextCityIdx;
         $scope.options.length = 0;
-        $scope.options = nextOptions.slice(0);
-        $scope.gameState = "question";        
-        $scope.loadNextQuestion();         
-    };
+        $scope.options = nextOptions.slice(0);  
+        $scope.loadNextQuestion();  
+    }
 
-    $scope.guess = function guess(answerIdx) {
+    function checkForAjax() { //http:stackoverflow.com/questions/951021/what-do-i-do-if-i-want-a-javascript-version-of-sleep
+        if ($scope.waitForAjax) {
+            setTimeout(checkForAjax, 50); 
+        }
+        else {
+            beginRoundPart2();
+        }
+    }
+
+    $scope.guess = function (answerIdx) {
         if(!guessTaken) {
             guessTaken = true;
             $scope.guessIdx = answerIdx;
@@ -69,68 +81,63 @@ quicklist.controller('gameController', function($scope, $http){
         }
     };
 
+    $scope.loadNextQuestion = function () {
+        nextOptions.length = 0;
+        for (var i=0; i<numOptions; i++) {
+            addAnswer(i);
+        }
+        nextCityIdx = Math.floor(Math.random() * numOptions);
+        var nextCity = nextOptions[nextCityIdx].city;
+        getTodaysWeather(nextCity);
+        shownCities.push(nextCity);
+    };  
+    
+    function addAnswer(idx) {
+        var randomCityIdx;
+        var randomCity;
+        do {
+            randomCityIdx = Math.floor(Math.random() * $scope.cities.length);
+            randomCity = $scope.cities[randomCityIdx];
+        }
+        while (cityAlreadyInOptionsOrAnswerList(randomCity));
+        
+        nextOptions.push({"id":idx, "city":randomCity});
+    }
 
-    getTodaysWeather = function getTodaysWeather(city){
-        $scope.ajaxLoading = true;
+    function cityAlreadyInOptionsOrAnswerList(city) {
+        var retVal = false;
+        for (var i=0; i<nextOptions.length; i++) {
+            if (nextOptions[i].city == city) {
+                retVal = true;
+            }
+        }
+        for (i=0; i<shownCities.length; i++) {
+            if (shownCities[i] == city) {
+                retVal = true;
+            }
+        }
+        return retVal;
+    }
+
+    function getTodaysWeather (city){
+        ajaxLoading = true;
         $http({method: 'GET', 
             url: "https://george-vustrey-weather.p.mashape.com/api.php?location=" + encodeURIComponent(city), 
             headers: {
                 "X-Mashape-Authorization":"Olg4sdyWgN698QCZ7BPD5OEOkJ0XiluN"
             }
         })
-            .success(function(data, status, headers, config) {
+        .success(function(data, status, headers, config) {
             nextCityWeather = data[0];
             nextCityWeather.high = Math.round(nextCityWeather.high);            
             nextCityWeather.low = Math.round(nextCityWeather.low);
-            $scope.ajaxLoading = false;
+            ajaxLoading = false;
             $scope.waitForAjax = false;
-            })
-            .error(function(data, status, headers, config) {
+        })
+        .error(function(data, status, headers, config) {
             $scope.status=status;
-            });
+            //TODO more intelligent error handling
+        });
+    }
 
-    };
-
-    // mockTodaysWeather = function mockTodaysWeather(city){
-    //     nextCityWeather.high = 99;            
-    //     nextCityWeather.low = 9;
-    //     $scope.ajaxLoading = false;
-    //     $scope.waitForAjax = false;
-    // };
-
-    cityNotAlreadyInOptionsOrAnswerList = function cityNotAlreadyInOptionsOrAnswerList(city) {
-        var retVal = true;
-        for (var i=0; i<nextOptions.length; i++) {
-            if (nextOptions[i].city == city) {
-                retVal=false;
-            }
-        }
-        for (i=0; i<shownCities.length; i++) {
-            if (shownCities[i] == city) {
-                retVal=false;
-            }
-        }
-        return retVal;
-    };
-
-    addAnswer = function addAnswer(idx) {
-        var randomCityIdx = Math.floor(Math.random() * $scope.cities.length); 
-        if(cityNotAlreadyInOptionsOrAnswerList($scope.cities[randomCityIdx])){
-            nextOptions.push({"id":idx, "city":$scope.cities[randomCityIdx]});
-        }
-        else {
-            addAnswer(idx);
-        }
-    };
-
-    $scope.loadNextQuestion = function loadNextQuestion() {
-        nextOptions.length = 0;
-        for (var i=0; i<numOptions; i++) {
-            addAnswer(i);
-        }
-        nextCityIdx = Math.floor(Math.random() * numOptions);
-        getTodaysWeather(nextOptions[nextCityIdx].city);
-        shownCities.push(nextOptions[nextCityIdx].city);
-    };  
-    
 });
